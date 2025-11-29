85e062dbe5ffc36deb3efe7e310c81584d72fa42
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2025-11-29 11:20:06 +0530

    feat: updating gemini api to send images

--- service/geminiApi.py
+++ service/geminiApi.py
@@ -1,10 +1,12 @@
 import google.generativeai
+from PIL import Image
 
 def sendToGemini(
     apiKey: str,
     message: str,
     sys_prompt: str,
     model: str = "gemini-2.0-flash",
+    image_data: Image = None,
 ):
     google.generativeai.configure(api_key=apiKey, transport="rest")
 
@@ -14,5 +16,9 @@ def sendToGemini(
     chat_args = {}
     convo = model_instance.start_chat(**chat_args)
 
-    convo.send_message(message)
+    content = [message]
+    if image_data:
+        content.append(image_data)
+
+    convo.send_message(content)
     return convo.last.text, convo.history
